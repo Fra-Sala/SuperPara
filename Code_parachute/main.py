ff9fb1e942c61b282d75f1a3fe6c1f4dbdd79143
Francesco Sala <francesco.sala@epfl.ch>
2023-03-04 11:15:24 +0100

    first lines of code

---
+++ Code_parachute/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+try:
+
+    design = int(input("Select a desired design:\n1) Disk-gap-band chute \n2) Ribbon "))
+#     consider that historically these are the only two types of parachutes effectivley used for Earth re-entry
+except ValueError:
+    print("You did not make an acceptable choice")
+
+
